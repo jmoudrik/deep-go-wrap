29556177d7db92f2633aea7f14272fc9b9fa4893
Joe Moudrik <j.moudrik@gmail.com>
2015-05-13 16:28:20 +0200

    utils: improve dist statistics

--- utils.py
+++ utils.py
@@ -12,23 +12,33 @@ def empty_board_mask(board):
                 a[row][col] = 0
     return a
 
+
 def dist_stats(dist, top=3):
+    def ind2pt(ind):
+        return ind / side, ind % side
+
+    def format_move_prob((row, col), prob):
+        return "%s  %.6f"%( gomill.common.format_vertex((row, col)), prob)
+
     assert dist.shape[1] == dist.shape[0]
     side = dist.shape[0]
-    
-    best_top_ind = np.argsort(dist.ravel())[-top:]
+
+    sort_ind = np.argsort(dist.ravel())
     ret = []
-    sum_prob = 0
-    for num, i in enumerate(reversed(best_top_ind)):
-        row, col = i / side, i % side
-        prob = 100 * dist[row][col]
-        sum_prob += prob
-        ret.append("%d: %s  %.1f%%"%(num + 1,
-                                      gomill.common.format_vertex((row, col)),
-                                      prob))
-                   
-    #ret.append("Top %d sum: %.1f%%"%(top, sum_prob))
-                   
+
+    # some statistics
+    row, col = ind2pt(sort_ind[0])
+    ret.append("mean: \t%.6f"%(np.mean(dist)))
+    ret.append("stddev: %.6f"%(np.std(dist)))
+    ret.append("min: \t" + format_move_prob((row, col), dist[row][col]))
+
+    # list of # top moves
+    for num, i in enumerate(reversed(sort_ind[-top:])):
+        row, col = ind2pt(i)
+        prob = dist[row][col]
+        ret.append("%d: \t"%(num+1) + format_move_prob((row, col), prob))
+
+
     return "\n".join(ret)
 
 
@@ -37,11 +47,11 @@ if __name__ ==  "__main__":
         a = np.random.random((3, 3))
         a[1][1] = 0
         a = a / a.sum()
-        
+
         print dist_stats(a)
-        
-        
-    
+
+
+
     test()
-        
-    
+
+
