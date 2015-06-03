1326da8c2c55b2dee61125bdfc62522f078c47c5
Joe Moudrik <j.moudrik@gmail.com>
2015-06-03 12:35:11 +0200

    deepgowrap: remove missing (& unused) import

--- deepgowrap.py
+++ deepgowrap.py
@@ -8,15 +8,14 @@ import sys
 import gomill
 from gomill import gtp_engine, gtp_states
 
-from players import * 
+from players import *
 
-from state import State
 import bot_deepcl
 
 def make_engine(player):
     """Return a Gtp_engine_protocol which runs the specified player."""
     gtp_state = gtp_states.Gtp_state(move_generator=player.genmove)
-    
+
     engine = gtp_engine.Gtp_engine_protocol()
     engine.add_protocol_commands()
     engine.add_commands(gtp_state.get_handlers())
@@ -29,7 +28,7 @@ def main_random():
     # player def
     # player = players.WrappingPassPlayer(players.RandomPlayer())
     player = DistWrappingMaxPlayer(RandomDistBot())
-    
+
     # player => engine => RUN
     engine = make_engine(player)
     gomill.gtp_engine.run_interactive_gtp_session(engine)
@@ -37,12 +36,12 @@ def main_random():
 def main_deepcl():
     logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
                         level=logging.DEBUG)
-    
+
     ## DCL_PATH = '/PATH/TO/YOUR/DeepCL'
     DCL_PATH = '/home/jm/prj/DeepCL/'
     deepcl_io = bot_deepcl.DeepCL_IO(os.path.join(DCL_PATH, 'build/predict'), options={
         ## set up your weights file
-        'weightsfile': os.path.join(DCL_PATH, "build/weights.dat"), 
+        'weightsfile': os.path.join(DCL_PATH, "build/weights.dat"),
         'outputformat': 'binary',
             })
     deepcl_bot = bot_deepcl.DeepCLDistBot(deepcl_io)
