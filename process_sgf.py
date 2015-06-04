116dd819a53d93e244e09e2edc0b562bfe8f3a35
Joe Moudrik <j.moudrik@gmail.com>
2015-06-04 15:06:01 +0200

    datasets: write boarsize to hdf dataset attributes

--- process_sgf.py
+++ process_sgf.py
@@ -213,6 +213,7 @@ def main():
                                   compression='gzip', compression_opts=9)
         
         dset_x.attrs['name'] = args.plane
+        dset_x.attrs['boardsize'] = args.boardsize
         dset_y.attrs['name'] = args.label
         dset_x.attrs['original_dtype'] = repr(sample_x.dtype)
         dset_y.attrs['original_dtype'] = repr(sample_x.dtype)
